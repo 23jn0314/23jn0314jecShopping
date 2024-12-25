8b01d4134baad605f6e9c0bcebd810afb3e9225c
23jn0314 <23jn0314@jec.ac.jp>
2024-12-25 11:28:37 +0900

    Update config.php

--- helpers/config.php
+++ helpers/config.php
@@ -1,6 +1,6 @@
 <?php
     // DB接続設定
-    define('DSN', 'tcp:23jn0314db.database.windows.net,1433');
+    define('DSN', 'sqlsrv:server=tcp:23jn0314db.database.windows.net,1433;database=23jn0314');
     define('DB_USER', 'jndb');
     define('DB_PASSWORD', 'Pa$$word1234');
 ?>
